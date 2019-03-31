fbacf81f032e19b77b3d41aac1662f3e741f1c4f
David Amador Moreno <amador.moreno.david@gmail.com>
2019-03-31 17:26:31 +0200

    Añadido datasource

--- lib/data/model/search/id.dart
+++ lib/data/model/search/id.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
+import 'package:youtube_search/data/model/serializer/serializers.dart';
 
 part 'id.g.dart';
 
